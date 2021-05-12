6867966a291c29856394871f5e777bdb5ac23d7f
Vladislav Lanets <vladislav.lanets@gmail.com>
2021-05-12 17:18:01 +0200

    Create method Run

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : Unit {
+
+    private float speedX;
+
+
+    private void  Awake() {
+        sprite = GetComponentInChildren<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        currentHealth = maxHealth;
+        rigidbody = GetComponent<Rigidbody2D>();
+        speedX = speed / 100;
+    }
+
+    private void Update()
+    {
+        animator.SetBool("inMove", false);
+
+        Run();
+    }
+
+    private void Run() {
+
+        
+
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            sprite.flipX = false;
+            animator.SetBool("inMove", true);
+            transform.Translate(speedX, 0, 0);
+        } else if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            sprite.flipX = true;
+            animator.SetBool("inMove", true);
+            transform.Translate(-speedX, 0, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour {
+    [SerializeField] protected int maxHealth;
+    protected int currentHealth;
+    [SerializeField] protected float speed;
+    protected SpriteRenderer sprite;
+    protected Animator animator;
+    protected Rigidbody2D rigidbody;
+
+    public virtual void TakeDamage(float damege)
+    {
+        Die();
+    }
+
+    protected virtual void Die() { }
+    
+}
