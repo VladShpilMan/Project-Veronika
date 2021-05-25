fb663ba956c7334eba79eeed718a087b7ef07b72
Vladislav Lanets <vladislav.lanets@gmail.com>
2021-05-25 23:26:24 +0200

    Particle leaf fall

---
+++ Assets/Scripts/Falling leaves.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fallingleaves : MonoBehaviour
+{
+    //void OnTriggerEnter2D(Collider2D col)
+    //{
+    //    Unit unit = collider.GetComponent<Unit>();
+
+    //    if (unit && unit is Character)
+
+    //}
+}
