1748f4102cec9ebc7c1b6acb67d2191115670fe0
Vladislav Lanets <vladislav.lanets@gmail.com>
2021-07-20 20:45:50 +0200

    Main Menu

--- Assets/Scripts/UI/ButtonsSound.cs
+++ Assets/Scripts/UI/ButtonsSound.cs
@@ -1,19 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonsSound : MonoBehaviour
 {
     [SerializeField] private AudioSource _myFx;
     [SerializeField] private AudioClip _hoverFx, _clickFx;
 
+    [SerializeField]private GameObject line;
+
     public void HoverSound()
     {
-        _myFx.PlayOneShot(_hoverFx, 0.3F);
+        _myFx.PlayOneShot(_hoverFx, 0.3F);       
     }
 
     public void ClickSound()
     {
         _myFx.PlayOneShot(_clickFx, 0.3F);
+    }
+
+    public void MouseEnter()
+    {
+        line.SetActive(true);
+    }
+
+    public void MouseExit()
+    {
+        line.SetActive(false);
     }
 }
